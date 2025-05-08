02b297cf8dc84b8f47599212ba744fe980ec2a6b
Taalib Asaf <87121983+taalib25@users.noreply.github.com>
2025-05-08 18:25:37 +0530

    Add PrayerTimes interface for managing prayer time data

---
+++ src/utils/types.tsx
@@ -0,0 +1,13 @@
+/**
+ * Prayer times interface
+ */
+export interface PrayerTimes {
+  fajr: string;
+  sunrise: string;
+  dhuhr: string;
+  asr: string;
+  maghrib: string;
+  isha: string;
+}
+
+
